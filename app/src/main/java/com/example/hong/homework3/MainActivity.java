bea421f7b3c70d14b14e91090d0eb7e8233df941
夜夏楓御 <wilson7016@gmail.com>
2017-04-11 01:39:27 +0800

    Homework3_end

--- app/src/main/java/com/example/hong/homework3/MainActivity.java
+++ app/src/main/java/com/example/hong/homework3/MainActivity.java
@@ -76,7 +76,6 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-
         switch (item.getItemId()){
             case R.id.action_settings:
 
